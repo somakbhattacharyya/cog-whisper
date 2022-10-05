758797e12b4a68a0942086b969502930dfa301a0
Chenxi <chenxi.whitehouse@gmail.com>
2022-10-05 23:13:34 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -8,6 +8,7 @@ wget https://openaipublic.azureedge.net/main/whisper/models/e4b87e7e0bf463eb8e69
 """
 
 import io
+import os
 from typing import Optional
 import torch
 from cog import BasePredictor, Input, Path, BaseModel
@@ -15,12 +16,15 @@ from cog import BasePredictor, Input, Path, BaseModel
 import whisper
 from whisper.model import Whisper, ModelDimensions
 from whisper.tokenizer import LANGUAGES
+from whisper.utils import format_timestamp
 
 
 class ModelOutput(BaseModel):
     detected_language: str
     transcription: str
     translation: Optional[str]
+    txt_file: Optional[Path]
+    srt_file: Optional[Path]
 
 
 class Predictor(BasePredictor):
@@ -39,26 +43,59 @@ class Predictor(BasePredictor):
                 self.models[model].load_state_dict(state_dict)
 
     def predict(
-            self,
-            audio: Path = Input(description="Audio file"),
-            model: str = Input(
-                default="base",
-                choices=["tiny", "base", "small", "medium", "large"],
-                description="Choose a Whisper model.",
-            ),
-            translate: bool = Input(
-                default=False,
-                description="Translate the text to English when set to True",
-            ),
+        self,
+        audio: Path = Input(description="Audio file"),
+        model: str = Input(
+            default="base",
+            choices=["tiny", "base", "small", "medium", "large"],
+            description="Choose a Whisper model.",
+        ),
+        transcription: str = Input(
+            choices=["plain text", "srt", "vtt"],
+            default="plain text",
+            description="Choose the format for the transcription",
+        ),
+        translate: bool = Input(
+            default=False,
+            description="Translate the text to English when set to True",
+        ),
     ) -> ModelOutput:
 
         """Run a single prediction on the model"""
         print(f"Transcribe with {model} model")
         model = self.models[model].to("cuda")
-        result_ori = model.transcribe(str(audio))
-        if not translate:
-            return ModelOutput(detected_language=LANGUAGES[result_ori["language"]], transcription=result_ori["text"])
+        result = model.transcribe(str(audio))
 
-        translation = model.transcribe(str(audio), task="translate")
-        return ModelOutput(detected_language=LANGUAGES[result_ori["language"]], transcription=result_ori["text"],
-                           translation=translation["text"])
+        if transcription == "plain text":
+            transcription = result["text"]
+        elif transcription == "srt":
+            transcription = write_srt(result["segments"])
+        else:
+            transcription = write_vtt(result["segments"])
+
+        if translate:
+            translation = model.transcribe(str(audio), task="translate")
+
+        return ModelOutput(
+            detected_language=LANGUAGES[result["language"]],
+            transcription=transcription,
+            translation=translation["text"] if translate else None,
+        )
+
+
+def write_vtt(transcript):
+    result = "WEBVTT\n"
+    for segment in transcript:
+        result += f"{format_timestamp(segment['start'])} --> {format_timestamp(segment['end'])}\n"
+        result += f"{segment['text'].strip().replace('-->', '->')}\n"
+    return result
+
+
+def write_srt(transcript):
+    result = ""
+    for i, segment in enumerate(transcript, start=1):
+        result += f"{i}\n"
+        result += f"{format_timestamp(segment['start'], always_include_hours=True, decimal_marker=',')} --> "
+        result += f"{format_timestamp(segment['end'], always_include_hours=True, decimal_marker=',')}\n"
+        result += f"{segment['text'].strip().replace('-->', '->')}\n"
+    return result
